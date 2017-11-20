6c1f130f3a6c3006ad153fa08ecf86344bbc4f0b
Hindia <mahmmudhindia@gmail.com>
2017-11-20 19:06:45 +0200

    Takes care of IOError, allows change of file

--- file.py
+++ file.py
@@ -1,29 +1,45 @@
-#easy python code for understanding read/write/append file
-import time#for using sleep() function
+import time
+try:
+	fname="notebook.txt"
+	handler=open(fname,"r")
+except IOError:
+	handler=open(fname,"a")
+	print("No default notebook was found, created one.")
+
 while True:
-	print("(1) Read the notebook\n(2) Add note\n(3) Empty the notebook\n(4) Quit\n")
-	choice=int(input("Please select one:"))
-	if choice==1:
-		handler=open("notebook.txt","r")
-		content=handler.read()
-		print(content)
-		handler.close()
-		continue
-	elif choice==2:
-		handler=open("notebook.txt","a")
-		text=input("write a new note:")
-		handler.write(text)
-		handler.close()
-		continue
-	elif choice==3:
-		handler=open("notebook.txt","w")
-		handler.close()
-		print("Notes deleted.")
-		continue
-	elif choice==4:
-		print("Notebook shutting down, thank you.")
-		time.sleep(1)
-		break
-	else:
-		print("Incorrect selection")
-		continue
+    print("Now using file",fname,"\n(1) Read the notebook\n(2) Add note\n(3) Empty the notebook\n(4) Change the notebook\n(5) Quit\n")
+    choice=int(input("Please select one:"))
+    if choice==1:
+        handler=open(fname,"r")
+        content=handler.read()
+        print(content)
+        handler.close()
+        continue
+    elif choice==2:
+        handler=open(fname,"a")
+        text=input("write a new note:")
+        date=time.strftime("%X %x")
+        text=text+":::written on/at "+date+"\n"
+        handler.write(text)
+        handler.close()
+        continue
+    elif choice==3:
+        handler=open(fname,"w")
+        handler.close()
+        print("Notes deleted.")
+        continue
+    elif choice==4:
+        fname=input("Give the name of the new file:")
+        try:
+            handler.close()
+            handler1=open(fname,"r")
+        except IOError:
+            handler1=open(fname,"a")
+            print("No notebook with that name detected, created one.")
+            continue		
+    elif choice==5:
+        print("Notebook shutting down, thank you.")
+        break
+    else:
+        print("Incorrect selection")
+        continue
